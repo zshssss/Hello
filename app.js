cfedb66ea71e8fa1efdc2411be4d0b00e475e35e
张少华 <18037472380@163.com>
2018-05-24 00:30:23 +0800

    allchange

---
+++ app.js
@@ -0,0 +1,17 @@
+/* 
+ 
+## git  init
+ # 
+ # git	add add + 文件名
+ # git  查看状态
+ #
+ # git *.html  上传所有html文件
+ #
+ # git rm --cached index.html 
+ # git add .    上传所有文件
+ # git commit -m '描述信息'
+ 
+ # 忽略
+ 
+ 
+ */ 
